40ce7e401aa0fb0d4c18c4eb65f854bdfe4e92dc
5huv0 <md88748874@gmail.com>
2024-05-20 02:08:56 +0600

    added lower case button and functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,6 +8,11 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleLowerCase = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
     const handleOnChange = (event)=>{
         // console.log("On Change");
         setText(event.target.value)
@@ -15,16 +20,21 @@ export default function TextForm(props) {
 
     const [text, setText] = useState('Enter text here');
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
-            <div className="mb-3">
+            <div className="mb-3 my-3">
                 <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myBox" rows="7"></textarea>
                 <div className="buttons">
                     <button type="button" className="btn btn-primary" onClick={handleUpCase}>To UpperCase</button>
-                    <button type="button" className="btn btn-secondary">To Lower Case</     button>
-                    <button type="button" className="btn btn-success">Remove extra gap</button>
+                    <button type="button" className="btn btn-secondary" onClick={handleLowerCase}>To Lower Case</button>
                 </div>
             </div>
         </div>
+        <div className='container'>
+            <h1>Your text total words and characters</h1>
+            <p>324 words & 3454 characters</p>
+        </div>
+        </>
   )
 }
